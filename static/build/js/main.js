9aac61af9c5829875d1b6e5b72548879cc596341
Albert Omoss <albertomoss@gmail.com>
2014-02-24 11:51:49 -0800

    moves google analytics code out into index.html

--- static/build/js/main.js
+++ static/build/js/main.js
@@ -119,14 +119,3 @@ window.onload = function(){
     var title = document.getElementById("title");
     title.classList.add('fadein');
 };
-
-// Google Analytics
-var _gaq = _gaq || [];
-_gaq.push(['_setAccount', 'UA-37217138-2']);
-_gaq.push(['_trackPageview']);
-
-(function() {
-var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-})();
